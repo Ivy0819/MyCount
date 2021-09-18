a96f3ca78aa532bf94b6d79a72e085714440f317
Ivy0819 <798603449@qq.com>
2021-09-18 21:06:32 +0800

    Initial commit

---
+++ app/src/main/java/com/swufestu/mycount/MainActivity.java
@@ -0,0 +1,55 @@
+package com.swufestu.mycount;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int count1 = 0;
+    int count2 = 0;
+    TextView score2;
+    TextView score3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        score2 = findViewById(R.id.textView2);
+        score3 = findViewById(R.id.textView3);
+    }
+    public void OnClick1(View btn){
+        Log.i("TAG", "Click:");
+        if(btn.getId()==R.id.button){//通过getid获得button，这样可以把所有的click放在一个方法里
+            count1 += 1;
+        }else if(btn.getId()==R.id.button2){
+            count1 += 2;
+        }else if(btn.getId()==R.id.button3){
+            count1 += 3;
+        }
+        score2.setText(count1+"");//一定要从int转为string类型，否则会报空值错误
+    }
+    public void OnClick2(View btn){
+        Log.i("TAG", "Click:");
+        if(btn.getId()==R.id.button5){//通过getid获得button，这样可以把所有的click放在一个方法里
+            count2 += 1;
+        }else if(btn.getId()==R.id.button6){
+            count2 += 2;
+        }else if(btn.getId()==R.id.button7){
+            count2 += 3;
+        }
+        score3.setText(count2+"");//一定要从int转为string类型，否则会报空值错误
+    }
+    public void OnClick3(View btn){
+        Log.i("TAG", "Click:");
+        count1 = 0;
+        count2 = 0;
+        score2.setText("0");
+        score3.setText("0");
+    }
+
+}
+
